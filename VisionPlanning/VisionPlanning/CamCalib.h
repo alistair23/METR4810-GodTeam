9a7d630dc8efe462f6c267de84ed721c986a62a4
Alistair Francis <alistair@alistair23.me>
2014-06-05 18:35:08 +1000

    Tidy up the final code
    These changes are after the final assessed demo and just tidy up the
    folder structure in preperation for open-sourcing
    
    Signed-off-by: Alistair Francis <alistair@alistair23.me>

--- VisionPlanning/VisionPlanning/CamCalib.h
+++
@@ -1,8 +0,0 @@
-#ifndef _INCLUDED_CAMCALIB_H
-#define _INCLUDED_CAMCALIB_H
-
-#include "Intrinsics.h"
-
-Intrinsics camCalib(char* settings, bool show_images);
-
-#endif
--- VisionPlanning/VisionPlanning/CommonFunctions.h
+++
@@ -1,8 +0,0 @@
-#ifndef _INCLUDED_COMMON_FUNCTIONS_H
-#define _INCLUDED_COMMON_FUNCTIONS_H
-
-// Returns milliseconds since the reference point
-long long time_now();
-
-
-#endif
